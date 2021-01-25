d4f1482ed7df02e628964e17a2259557c7594cc5
DavidCarbon <open-source@davidcarbon.dev>
2021-01-25 13:31:37 -0800

    Add a NullSafe and Non-Numerical Values Check
    Added a Check to Prevent non Numbers to pass through intergers functions

--- Core/CheatList.php
+++ Core/CheatList.php
@@ -1,17 +1,35 @@
 <?php
 function CheatType($string) {
-    $i = 0;
-    $cheattype = array("TreasureHuntReveal", "ProfileMasker", "NoCops", "DriftMods", "Wallhack", "TankMode", "SmoothWalls", "Speedhack", "FastPowerups", "MultiHack");
-    $detected = str_split(sprintf("%10d", decbin($string)));
-            
-    foreach($detected as $int) {
-        if($int == 1) {
-            $ret[] = $cheattype[$i];
-        }
-                
-        $i++;
+    if($string == 0) {
+        return "Unknown: [0]";
     }
-
-    return implode(", ", $ret);
+    elseif(is_numeric($string) == TRUE) {
+        if($string <= 1023) {
+            $i = 0;
+            $cheattype = array("TreasureHuntReveal", "ProfileMasker", "NoCops", "DriftMods", "Wallhack", "TankMode", "SmoothWalls", "Speedhack", "FastPowerups", "MultiHack");
+            $detected = str_split(sprintf("%10d", decbin($string)));
+            
+            foreach($detected as $int) {
+                if($int == 1) {
+                    $ret[] = $cheattype[$i];
+            }
+                
+            $i++;
+            }
+            
+            return implode(", ", $ret);
+        }
+        else {
+            return "Unknown: [".$string."]";
+        }
+    }
+    else {
+        if(!empty($string)) {
+            return "Unknown: [".$string."]";
+        }
+        else {
+            return "Unknown: [NULL]";
+        }
+    }
 }
 ?>
